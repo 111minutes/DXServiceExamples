46ec9438fa2bf2319352faf81af4a53a63af2b93
Sergey Zenchenko <zen@111min.com>
2012-11-06 18:37:00 +0200

    Initial Commit

---
+++ TwitterServiceApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TwitterServiceApp
+//
+//  Created by zen on 11/6/12.
+//  Copyright (c) 2012 111Minutes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
